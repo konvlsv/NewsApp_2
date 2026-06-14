e7e9e1897a6d2fcafa35e6bb0501782bfb996e2e
konstantinvasilev <kosvasl09@gmail.com>
2026-06-15 08:28:14 +0900

    Configure Hilt, add internet permission, and set up navigation host
    - Create `App` class annotated with `@HiltAndroidApp`.
    - Register `App` class and add `INTERNET` permission in `AndroidManifest.xml`.
    - Implement initial `AppNavHost` composable with `rememberNavController`.
    - Update `MainActivity` to host `AppNavHost` and remove boilerplate greeting code.

--- app/src/main/java/com/example/newsapp_2/MainActivity.kt
+++ app/src/main/java/com/example/newsapp_2/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.Surface
 import com.example.newsapp_2.ui.theme.NewsApp_2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NewsApp_2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface() {
+                    AppNavHost()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NewsApp_2Theme {
-        Greeting("Android")
     }
 }
