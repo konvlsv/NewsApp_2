367b79c5906cd7e63cddaca45d4ec680c3cdbc81
konstantinvasilev <kosvasl09@gmail.com>
2026-06-26 15:06:48 +0900

    Define news categories and add corresponding string resources
    - Create `NewsCategory` enum class in the domain layer.
    - Add string resources for all news categories in `strings.xml`.

---
+++ app/src/main/java/com/example/newsapp_2/domain/models/NewsCategory.kt
@@ -0,0 +1,23 @@
+package com.example.newsapp_2.domain.models
+
+enum class NewsCategory {
+    ALL,
+    BREAKING,
+    BUSINESS,
+    CRIME,
+    DOMESTIC,
+    EDUCATION,
+    ENTERTAINMENT,
+    ENVIRONMENT,
+    FOOD,
+    HEALTH,
+    LIFESTYLE,
+    POLITICS,
+    SCIENCE,
+    SPORTS,
+    TECHNOLOGY,
+    TOP,
+    TOURISM,
+    WORLD,
+    OTHER
+}
